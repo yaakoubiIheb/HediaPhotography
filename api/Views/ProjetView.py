a923f72eea01b64f31b5d68c114531272972a1b2
Yakoubi Iheb <iheb.yaakoubi@sesame.com.tn>
2022-05-01 18:01:15 +0100

    v1.1

--- api/Views/ProjetView.py
+++ api/Views/ProjetView.py
@@ -1,6 +1,8 @@
-from ..serializers import ProjetSerializer
-from ..models import Projet
-from django.http import HttpResponse, JsonResponse
+
+import json
+from ..serializers import ProjetSerializer,ImageSerializer,ViedoSerializer
+from ..models import Projet,Image,Video
+from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
 
@@ -36,14 +38,69 @@ def ProjetByName(request, pk):
     if request.method == 'GET':
         projets = Projet.objects.get(pk=pk)
         serializer = ProjetSerializer(projets)
-        res={"projet":[]}
+        images = Image.objects.filter(projet=pk)
+        imageSerializer = ImageSerializer(images,many=True)
+        videos = Video.objects.filter(projet=pk)
+        videoSerializer = ViedoSerializer(videos,many=True)
+
+
+
+        res={"projet":[],
+        "images":[],
+        "videos":[]
+        }
+
         
+        for video in videoSerializer.data:
+            
+            res["videos"].append({"id": video["id"],
+                "url":video["url"],
+                })
+        
+
+
+        for image in imageSerializer.data:
+             if image["priorite"]==2:
+                 url=request.META['HTTP_HOST']+image["image"] 
+
+
+                 res["images"].append({"id": image["id"],
+                "projet":image["projet"],
+                "priorite":image["priorite"],
+                "image":url})
+
+
+        for image in imageSerializer.data:
+             if image["priorite"]==1:
+                 url=request.META['HTTP_HOST']+image["image"] 
+
+
+                 res["images"].append({"id": image["id"],
+                "projet":image["projet"],
+                "priorite":image["priorite"],
+                "image":url})
+
+
+
+        for image in imageSerializer.data:
+             if image["priorite"]==0:
+                 url=request.META['HTTP_HOST']+image["image"] 
+
+
+                 res["images"].append({"id": image["id"],
+                "projet":image["projet"],
+                "priorite":image["priorite"],
+                "image":url})
+
     
         url=request.META['HTTP_HOST']+serializer.data["couverture"]   
         res["projet"].append({"nom": serializer.data["nom"],
              "description":serializer.data["description"],
              "theme":serializer.data["theme"],
             "couverture":url})
+
+        
+     
 
         
         return JsonResponse(res, safe=False)
