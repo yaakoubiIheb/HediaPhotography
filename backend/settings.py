97eec5e6a131c12132eb09501ab3e1212627c1c7
Yakoubi Iheb <iheb.yaakoubi@sesame.com.tn>
2022-06-28 21:48:34 +0100

    production

--- backend/settings.py
+++ backend/settings.py
@@ -116,13 +116,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+STATICFILES_DIRS=[BASE_DIR.joinpath('static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+MEDIA_ROOT = BASE_DIR.joinpath('static/images')
 MEDIA_URL = '/images/'
-
-
